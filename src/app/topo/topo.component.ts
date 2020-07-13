46886e6b5fe2aecb3ce78dca1a0740eb202b56b4
EullerHenrique <eullerhenrique@outlook.com>
2020-07-13 16:57:45 -0300

    Update

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -2,7 +2,8 @@ import {Component} from "@angular/core";
 
 @Component({
   selector: 'app-topo',
-  templateUrl: './topo.component.html'
+  //templateUrl: './topo.component.html'
+  template: "<p>Esse é o componente Topo</p>",
 
 })
 
