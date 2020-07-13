f71ea86b71e48f45ca20272c312cf85618dec89b
EullerHenrique <eullerhenrique@outlook.com>
2020-07-13 17:19:55 -0300

    Update

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -1,9 +1,35 @@
 import {Component} from "@angular/core";
 
 @Component({
-  selector: 'app-topo',
-  //templateUrl: './topo.component.html'
-  template: "<p>Esse é o componente Topo</p>",
+  selector: '.app-topo',//Tag: app-topo, Atributo: [app-topo], Classe: .app-topo
+  templateUrl: './topo.component.html',
+
+  /*
+
+  Template inline
+  template: `
+    <p>
+      Esse é o componente Topo
+    </p>
+  `,
+
+   */
+
+  styleUrls: ['./topo.component.css']
+
+  /*
+
+  Style inline
+
+  styles:
+    [`
+        .exemplo {
+          color:red
+        }
+    `]
+
+  */
+
 
 })
 
