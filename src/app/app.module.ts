cba4ed2514837560ecc2d55a8db15f765965a44e
EullerHenrique <eullerhenrique@outlook.com>
2020-07-13 19:30:41 -0300

    Criação dos componentes tentativas e progresso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {TopoComponent} from "./topo/topo.component";
+import { PainelComponent } from './painel/painel.component';
+import { TentativasComponent } from './tentativas/tentativas.component';
+import { ProgressoComponent } from './progresso/progresso.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopoComponent
+    TopoComponent,
+    PainelComponent,
+    TentativasComponent,
+    ProgressoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/painel/painel.component.ts
@@ -0,0 +1,9 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-painel',
+  templateUrl: './painel.component.html',
+  styleUrls: ['./painel.component.css']
+})
+
+export class PainelComponent  {}
