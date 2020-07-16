b1f9fc8339d11f95cc5ef7dd665eed5a29f96715
EullerHenrique <eullerhenrique@outlook.com>
2020-07-16 19:53:10 -0300

    feat: adds the attributes coracaoVazio and CoracaoCheio

--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -5,7 +5,10 @@ import { Component} from '@angular/core';
   templateUrl: './tentativas.component.html',
   styleUrls: ['./tentativas.component.css']
 })
+
 export class TentativasComponent{
 
-
+  public coracaoVazio: string = "/assets/coracao_vazio.png";
+  public coracaoCheio: string = "/assets/coracao_cheio.png";
+  
 }
