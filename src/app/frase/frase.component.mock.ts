fef47b0fe2caba273a4089656e464c6e609910d5
EullerHenrique <eullerhenrique@outlook.com>
2020-07-17 02:44:12 -0300

    feat: creates the mock of the frase" class

---
+++ src/app/frase/frase.component.mock.ts
@@ -0,0 +1,37 @@
+import {Frase} from "./frase.component";
+
+
+/* O que é mock?
+
+  Bom, quando falamos de teste de unidade, queremos testar as coisas isoladamente sem envolver outros componentes,
+  mas o que acontece se seu componente (no caso do angular por exemplo) precisa de outro componente pra funcionar?
+
+  Se você testar um componente que usa outro componente, esse não é mais um teste unitário por que envolve mais
+  de uma unidade. Esse é o teste de integração.
+
+  Nesse caso, como transformamos um teste de integração em um teste de unidade? Simulando o comportamento
+  do segundo componente. Essa simulação é justamente o Mock.
+
+  O Mock é um cara capaz de simular o comportamento de um componente, tornando possível você testar um
+  outro componente isoladamente.
+
+  No Angular, onde isso pode estar? Bom, digamos que seu componente lista e ordena um grupo de dados (usuários por exemplo).
+  Como você testa a ordenação e a listagem sem pedir isso direto pra API? Você usa um json local mesmo.
+  Isso já poderia ser chamado de Mock, você está simulando os dados da API.
+
+  Esse exemplo é muito básico, só pra te dar a ideia mais ou menos do que é.
+  Você pode "mockar" várias coisas: dados, comportamentos, etc..
+
+
+ */
+
+//Mock
+const FRASES: Frase[] =
+  [
+
+    new Frase('I like to learn', 'Eu gosto de aprender'),
+    new Frase('I watch tv', 'Eu assisto tv'),
+    new Frase('How are you? ', 'Como vai voçê?'),
+    new Frase('I eat bread',  'Eu como pão')
+
+  ];
