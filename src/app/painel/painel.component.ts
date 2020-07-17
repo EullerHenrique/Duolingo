3956a18a4eea32d17db13c3b8557a9e3b7440d63
EullerHenrique <eullerhenrique@outlook.com>
2020-07-17 03:06:13 -0300

    feat: creates the "frases" and "intrucao" attributes

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,9 +1,22 @@
 import { Component} from '@angular/core';
 
+import { Frase} from "../frase/frase.component";
+import { FRASES } from "../frase/frase.component.mock"
+
 @Component({
   selector: 'app-painel',
   templateUrl: './painel.component.html',
   styleUrls: ['./painel.component.css']
 })
 
-export class PainelComponent  {}
+export class PainelComponent {
+
+  public frases: Frase[] = FRASES;
+  public instrucao: string = "Traduza a frase: ";
+
+  constructor() {
+
+
+  }
+
+}
