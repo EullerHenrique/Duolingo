0dc67839781c2f451060fcc26cdf03b2baf29cc0
EullerHenrique <eullerhenrique@outlook.com>
2020-07-21 01:30:48 -0300

    feat: adds the Output decorator in the endGame attribute and emits the victory and defeat events

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 import {Frase} from "../shared/frase.model";
 import {FRASES} from "../shared/frase.model.mock";
@@ -20,6 +20,11 @@ export class PainelComponent {
   public progresso: number = 0;
   public tentativas: number = CORACOES.length;
 
+  //O decorator output decora o atributo encerrarJogo (possui como valor a instância de um emissor de eventos),
+  //tal decoração permite que esse atributo seja acessado pelo componente pai
+
+  @Output() public encerrarJogo: EventEmitter<string> = new EventEmitter();
+
   public atualizarResposta(resposta: Event): void{
     this.resposta = ((<HTMLInputElement>resposta.target).value);
   }
@@ -28,9 +33,11 @@ export class PainelComponent {
 
     if(this.resposta.trim() == this.frase.frasePtBr){
 
-      //Avança para a próxima frase
+      //Adiciona um acerto
 
       this.i++;
+
+      //Avança para a próxima frase
 
       this.atualizarFrase();
 
@@ -38,14 +45,22 @@ export class PainelComponent {
 
       this.progresso+=(100/this.frases.length);
 
+      //Vitória
+
+      if(this.i == this.frases.length){
+        this.encerrarJogo.emit('Vitória'); // Emite o evento Vitória para o componente pai
+      }
+
     }else{
 
       //Elimina uma tentativa, ou seja, um coração
 
       this.tentativas--;
 
+      // Derrota
+
       if(this.tentativas == -1){
-        alert("GAME OVER");
+        this.encerrarJogo.emit('Derrota'); //Emite o evento Derrota para o componente pai
       }
 
     }
@@ -53,6 +68,7 @@ export class PainelComponent {
   }
 
   public atualizarFrase():void {
+
     this.frase = this.frases[this.i];
 
     //Limpar a resposta
